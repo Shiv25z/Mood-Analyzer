4f34cab3d0d762e21124894bf562aa436f3f95b7
Shiv25z <shivamzagade25899@gmail.com>
2022-10-27 19:59:22 +0530

    Welcome to Mood Analyzer Problem!

---
+++ main/java/com/moodanalyzer/MoodAnalyzer.java
@@ -0,0 +1,10 @@
+package com.moodanalyzer;
+
+
+
+public class MoodAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("-----------------------Welcome to Mood Analysis Problem!-----------------------i");
+    }
+
+}
---
+++ test/java/com/moodanalzyer/MoodAnalyzerTest.java
@@ -0,0 +1,14 @@
+package com.moodanalzyer;
+
+import com.moodanalyzer.MoodAnalyzer;
+import org.junit.jupiter.api.Test;
+
+public class MoodAnalyzerTest {
+    @Test
+    void testMoodAnalysis() throws Exception{
+        MoodAnalyzer mood = new MoodAnalyzer();
+
+
+
+    }
+}
