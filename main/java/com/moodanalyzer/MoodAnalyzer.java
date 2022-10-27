2916845b1d1f726e216eed9be132398fc37d2c82
Shiv25z <shivamzagade25899@gmail.com>
2022-10-27 20:09:45 +0530

    Added: Test Cases to Analyze Happy or Sad Mood

--- main/java/com/moodanalyzer/MoodAnalyzer.java
+++ main/java/com/moodanalyzer/MoodAnalyzer.java
@@ -1,9 +1,13 @@
 package com.moodanalyzer;
 
-
-
 public class MoodAnalyzer {
-    public static void main(String[] args) {
+
+    public String analyzer(String message){
+        if (message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }void main(String[] args) {
         System.out.println("-----------------------Welcome to Mood Analysis Problem!-----------------------i");
     }
 
--- test/java/com/moodanalzyer/MoodAnalyzerTest.java
+++ test/java/com/moodanalzyer/MoodAnalyzerTest.java
@@ -1,14 +1,15 @@
 package com.moodanalzyer;
 
 import com.moodanalyzer.MoodAnalyzer;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MoodAnalyzerTest {
+
     @Test
     void testMoodAnalysis() throws Exception{
         MoodAnalyzer mood = new MoodAnalyzer();
-
-
-
+        String s =  mood.analyzer("I am in Sad mood");
+        Assertions.assertEquals(s, "SAD");
     }
 }
